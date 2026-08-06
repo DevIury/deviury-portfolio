80d1cf2a1aaf582bb0202499bcb3b1c8267c6419
Iury Gabriel <contato.iury.dev@gmail.com>
2026-08-06 10:53:05 -0300

    feat: update domain from deviury.com to deviury.com.br

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://deviury.com',
+	site: 'https://deviury.com.br',
 	compressHTML: true,
 	integrations: [
 		sitemap(),
