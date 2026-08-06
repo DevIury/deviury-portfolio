e26497b84aec97226a59c6ee995c78d6eafc52c1
Iury Gabriel <contato.iury.dev@gmail.com>
2026-08-06 12:25:47 -0300

    fix: disable compressHTML to fix empty HTML output

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	site: 'https://deviury.pages.dev',
-	compressHTML: true,
+	compressHTML: false,
 	integrations: [
 		sitemap(),
 	],
