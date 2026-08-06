6b8d91476bb3ae7e79574e05d826222155bb318c
Iury Gabriel <contato.iury.dev@gmail.com>
2026-08-06 11:32:17 -0300

    chore: use deviury.pages.dev temporarily until .com.br domain is purchased

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://deviury.com.br',
+	site: 'https://deviury.pages.dev',
 	compressHTML: true,
 	integrations: [
 		sitemap(),
